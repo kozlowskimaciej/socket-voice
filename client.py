b8416f1a48451dab49e8e65d5029db93aa259144
Maciej Kozłowski <maciejmaciejkoz@gmail.com>
2023-04-18 17:35:23 +0200

    Make working prototype

--- client.py
+++ client.py
@@ -1,28 +1,27 @@
 import socket
-import io
-
-from PIL import Image
+import sounddevice as sd
+import numpy as np
+import pickle
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_address = ("localhost", int(input('Port = ')))
 sock.connect(server_address)
 
+def callback(indata: np.ndarray, frames, time, status):
+    data = pickle.dumps(indata)
+    size = len(data).to_bytes(4, byteorder='big')
+    sock.send(size)
+    sock.send(data)
+    pass
+
+rate = 8000
+blocksize = int(rate/10)
+stream = sd.InputStream(callback=callback, blocksize=blocksize, samplerate=rate, dtype='int16', channels=1)
+
+stream.start()
+
 while True:
-    # Read the size of the image data
-    sock.send(b"Fetching")
-    size_bytes = sock.recv(4)
-    size = int.from_bytes(size_bytes, byteorder='big')
+    pass
 
-    # Read the image data
-    img_byte_arr = b''
-    while len(img_byte_arr) < size:
-        img_byte_arr += sock.recv(1024)
-
-    sock.send(b"Ready")
-    image = Image.open(io.BytesIO(img_byte_arr))
-    image.save('test.jpeg')
-
-
-# print(data)
-sock.close()
+# sock.close()
--- server.py
+++ server.py
@@ -1,10 +1,9 @@
 import socket
 import threading
 import struct
-import io
 import time
-
-import pyautogui
+import sounddevice as sd
+import pickle
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -19,42 +18,29 @@ sock.listen()
 
 client_threads = []
 
-
 def handle_client(client_socket):
     client_socket.setsockopt(
         socket.SOL_SOCKET, socket.SO_LINGER, struct.pack("ii", 1, 0)
     )
 
-    while True:
-        sent_t = time.perf_counter()
+    with sd.OutputStream(samplerate=8000, blocksize=800, dtype='int16', channels=1) as stream:
+        while True:
+            sent_t = time.perf_counter()
 
-        data = client_socket.recv(1024)
+            size_bytes = client_socket.recv(4)
+            size = int.from_bytes(size_bytes, byteorder='big')
 
-        screenshot = pyautogui.screenshot()
+            data = client_socket.recv(int(size))
 
-        # Convert the image to JPEG format
-        screenshot = screenshot.convert('RGB')
+            data = pickle.loads(data)
+            print(len(data))
 
-        # Resize the image to a smaller size (optional)
-        screenshot = screenshot.resize((1920, 1080))
+            stream.write(data)
 
-        img_byte_arr = io.BytesIO()
+            print(time.perf_counter() - sent_t)
 
-        screenshot.save(img_byte_arr, format='JPEG', quality=30)
-        img_byte_arr = img_byte_arr.getvalue()
 
-        size = len(img_byte_arr)
-        client_socket.send(size.to_bytes(4, byteorder='big'))
-
-        try:
-            client_socket.send(img_byte_arr)
-        except BrokenPipeError:
-            break
-
-        print(time.perf_counter() - sent_t)
-
-    client_socket.close()
-
+    # client_socket.close()
 
 while True:
     # Wait for a connection
